c8622ba6406d74585620539a1dd58fffc5b8b3d3
Idris Westbrook <westbrook.idris@gmail.com>
2022-06-08 15:48:59 -0400

    add default conversion

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 const list = document.querySelectorAll('.list select');
 
-for (let i = 0; i < list.length; i++) {
+for(let i = 0; i < list.length; i++) {
     for(currencyList in countryList) {
-        //Create a variable that passes the currency as a text value
-        let option = `<option value='${currencyList}'>${currencyList}</option>`;
+        //Set USD to GBP as default conversion
+        let selected;
+        if(i == 0) {
+            selected = currencyList == 'USD' ? 'selected' : '';
+        } else if(i == 1){
+            selected = currencyList == 'GBP' ? 'selected' : '';
+        }
+        //Create an option tag that passes the currency as a text value
+        let option = `<option value='${currencyList}' ${selected}>${currencyList}</option>`;
+        //Insert the option tag into the select tag
         list[i].insertAdjacentHTML('beforeend', option);
     }
 }
